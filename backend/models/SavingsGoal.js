2dadea6bae3be1a857b56e2d738c1e8ce0878b76
Shubham Priyam <2025ca93520@gmail.com>
2026-08-20 10:36:25 +0530

    clean build

--- backend/models/SavingsGoal.js
+++ backend/models/SavingsGoal.js
@@ -7,23 +7,6 @@ export class SavingsGoal {
     this.currentAmount = currentAmount;
   }
 
-  static remainingAmount(goal) {
-    return Math.max(0, goal.targetAmount - goal.currentAmount);
-  }
-
-  static progressPercentage(goal) {
-    if (!goal.targetAmount) return 0;
-    return Math.min(100, Math.round((goal.currentAmount / goal.targetAmount) * 100));
-  }
-
-  static withProgress(goal) {
-    return {
-      ...goal,
-      remainingAmount: SavingsGoal.remainingAmount(goal),
-      progressPercentage: SavingsGoal.progressPercentage(goal),
-    };
-  }
-
   static validateFunds(data, goal) {
     const errors = [];
     const amount = Number(data.amount);
